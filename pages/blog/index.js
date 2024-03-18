a91bc4b80b872b080f470051b4df41c107a22674
Admin <admin@Admins-MBP.attlocal.net>
2024-03-18 11:46:40 -0500

    First article

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -32,7 +32,7 @@ export default function AboutPage({
               as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
               href={`/posts/[year]/[slug]`}
             >
-              <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <span className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
                 {post.data.date && (
                   <p className="uppercase mb-3 font-bold opacity-60">
                     {post.data.date}
@@ -45,7 +45,7 @@ export default function AboutPage({
                   </p>
                 )}
                 <ArrowIcon className="mt-4"/>
-              </a>
+              </span>
             </Link>
           </li>
         ))}
--- pages/posts/[year]/[slug].js
+++ pages/posts/[year]/[slug].js
@@ -67,7 +67,7 @@ export default function PostPage({
           <div className="grid md:grid-cols-2 lg:-mx-24 mt-12">
             {prevPost && (
               <Link href={`/posts/${prevPost.slug}`}>
-                <a className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
+                <span className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
                   <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                     Previous
                   </p>
@@ -75,12 +75,12 @@ export default function PostPage({
                     {prevPost.title}
                   </h4>
                   <ArrowIcon className="transform rotate-180 mx-auto md:mr-0 mt-auto" />
-                </a>
+                </span>
               </Link>
             )}
             {nextPost && (
               <Link href={`/posts/${nextPost.slug}`}>
-                <a className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
+                <span className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
                   <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                     Next
                   </p>
@@ -88,7 +88,7 @@ export default function PostPage({
                     {nextPost.title}
                   </h4>
                   <ArrowIcon className="mt-auto mx-auto md:ml-0" />
-                </a>
+                </span>
               </Link>
             )}
           </div>
