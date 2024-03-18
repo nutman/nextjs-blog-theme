4284fb67730d5835fd68b5651c52b87bed85cc37
Admin <admin@Admins-MBP.attlocal.net>
2024-03-18 12:19:56 -0500

    First article

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header({name}) {
   return (
     <>
       <header className="flex flex-row items-center px-6 sm:px-12 py-6">
-        <div className="font-bold font-mono mr-6"><a href="/">LOBAN</a></div>
+        <div className="font-bold font-mono mr-6"><Link href="/">LOBAN</Link></div>
         <nav className="">
           <span className="hover:bg-gray-400 rounded py-1 px-2">
             <Link href="/">
