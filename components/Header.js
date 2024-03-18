4dd69a6954a1e4ccd0c8e2cd08d98cd9be110e40
Admin <admin@Admins-MBP.attlocal.net>
2024-03-18 11:51:28 -0500

    First article

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,30 @@
 import Link from 'next/link';
 
-export default function Header({ name }) {
+export default function Header({name}) {
   return (
     <>
       <header className="flex flex-row items-center px-6 sm:px-12 py-6">
         <div className="font-bold font-mono mr-6"><a href="/">LOBAN</a></div>
         <nav className="">
-          <Link href="/">
-            <span className="hover:bg-gray-400 rounded py-1 px-2">About</span>
+          <span className="hover:bg-gray-400 rounded py-1 px-2">
+            <Link href="/">
+            About
           </Link>
+          </span>
+          <span className="hover:bg-gray-400 rounded py-1 px-2">
           <Link href="/blog">
-            <span className="hover:bg-gray-400 rounded py-1 px-2">Blog</span>
-          </Link>
+
+            Blog
+        </Link>
+      </span>
           {/*<Link href="/projects">*/}
           {/*  <a className="hover:bg-gray-400 rounded py-1 px-2">Projects</a>*/}
           {/*</Link>*/}
         </nav>
-        <hr className="mt-4 opacity-20" />
+        <hr className="mt-4 opacity-20"/>
       </header>
     </>
 
-  );
+  )
+    ;
 }
