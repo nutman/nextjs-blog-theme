c8089a8149773ce3275ab62f96a6f3aeb350ccef
Admin <admin@Admins-MBP.attlocal.net>
2024-03-18 21:41:17 -0500

    Add analitics

--- components/SEO.js
+++ components/SEO.js
@@ -1,20 +1,10 @@
 import Head from 'next/head';
-
-/* eslint-disable */
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function SEO({ title, description }) {
   return (
     <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WY88M20WM5"></script>
-      <script>
-        // eslint-disable
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-WY88M20WM5');
-      </script>
-
+      <GoogleAnalytics gaId="G-WY88M20WM5" />
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
