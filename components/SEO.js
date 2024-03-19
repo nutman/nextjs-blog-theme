9a5f626b6f3da5d31ff3125764ea690a0a6232cc
Admin <admin@Admins-MBP.attlocal.net>
2024-03-18 21:20:38 -0500

    Add analitics

--- components/SEO.js
+++ components/SEO.js
@@ -3,7 +3,6 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
-      <!-- Google tag (gtag.js) -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-WY88M20WM5"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
